79de7cb4248f017d0a8fb29f2c0749f014b3dd51
Matt Yaraskavitch <myaraskavitch@softwaresecured.com>
2017-09-14 08:47:42 -0400

    Dummy content for initial stack debugging

---
+++ WebContent/colour.jsp
@@ -0,0 +1,7 @@
+<%@ include file="header.jsp" %>
+
+<h1>
+	<%= request.getParameter("colour") %>!
+</h1>
+
+<%@ include file="footer.jsp" %>
--- src/Dummy.java
+++ src/Dummy.java
@@ -1,6 +1,6 @@
 import java.io.IOException;
-import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -9,20 +9,10 @@ import javax.servlet.http.HttpServletResponse;
 public class Dummy extends HttpServlet {
 	
 	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException{
-		String colour = req.getParameter("colour");
-		PrintWriter out = res.getWriter();
-	
-		out.println (
-			      "<html> \n" +
-			        "<head> \n" +
-			          "<title> My first jsp  </title> \n" +
-			        "</head> \n" +
-			        "<body> \n" +
-			          "<font size=\"12px\">" +
-			            colour +
-			          "</font> \n" +
-			        "</body> \n" +
-			      "</html>" 
-			    );  
+		
+		RequestDispatcher reqDis;
+		reqDis = req.getRequestDispatcher("/colour.jsp");
+		
+		reqDis.forward(req, res);
 	}
 }
