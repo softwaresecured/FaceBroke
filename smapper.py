714c7aec44c7c76b2d50230429b83a6c2b9f5fd8
Matt Yaraskavitch <myaraskavitch@softwaresecured.com>
2017-10-13 13:12:42 -0400

    Work on python code to rebuild the virtual jsp file

--- smapper.py
+++ smapper.py
@@ -7,6 +7,9 @@ import re
 from pathlib import Path
 
 
+SEP = "------------------------------------------------------------------"
+
+
 # Retrieve list of complete absolute paths of files given extension, ignoring
 # out directory
 def getFilesofType(rootPath, extension):
@@ -19,22 +22,107 @@ def getFilesofType(rootPath, extension):
     return results
 
 
-def buildJspMap(fullPaths):
-    results = dict()
+# Now have two dicts, one of JSP names and paths and another
+# of smap names and paths
+# for name in smaps.keys():
+#    with smaps[name].open() as file:
+#        for line in file:
+#            print(line.strip())
+def load_jsp(filename, jsps):
+    result = list()
+    with jsps[filename].open() as file:
+        for line in file:
+            if ".jsp" in line and "include" in line:
+                matches = re.search(r"[\'\"](.*.jsp)", line)
+                if matches:
+                    chunk = load_jsp(matches.group(1), jsps)
+                    result = result + chunk
+            else:
+                result.append((-1, line.strip()))
+    return result
 
-    for p in fullPaths:
-        results[p.name()] = p
 
-    return results
+def printVirtFile(file):
+    print(" JAVA |  JSP | Line")
+    print(SEP)
+    for (index, line) in enumerate(file):
+        if line[0] != -1:
+            print(" {:4d} : {:4d} : {}".format(line[0], index+1, line[1]))
+        else:
+            print("      : {:4d} : {}".format(index+1, line[1]))
 
 
-def buildFullJsp(jspMap):
-    results = dict()
+def load_smap_jsp(jsp_name, virt_jsp, smaps, jsps):
+    pattern = r''
+    for piece in jsp_name.split("."):
+        if "_" in piece:
+            for chunk in piece.split("_"):
+                pattern += chunk + ".*"
+        else:
+            pattern += piece + ".*"
 
-    for f in jspMap.keys():
-        print(f)
+    for name in smaps:
+        if re.search(pattern, name):
+            print("JSP: {}    SMAP: {}".format(jsp_name, name))
+            with smaps[name].open() as smap:
+                files = list()
+                context = 0
+                main_jsp_id = -1
 
-    return results
+                for line in smap:
+                    if "+" in line:
+                        chunks = line.split()
+                        import_name = chunks[2]
+                        num_lines = 0
+                        with jsps[jsp_name].open() as file:
+                            for line in file:
+                                num_lines += 1
+                        files.append((import_name, num_lines))
+                        if import_name == jsp_name:
+                            main_jsp_id = len(files)-1
+                    elif ":" in line:
+                        input_start_line = None
+                        line_file_id = None
+                        repeat_count = None
+                        output_start_line = None
+                        output_line_increment = None
+
+                        chunks = line.split(":")
+                        jsp_piece = chunks[0].split(",")
+                        java_piece = chunks[1].split(",")
+                        if "#" in jsp_piece[0]:
+                            input_start_line = int(jsp_piece[0].split("#")[0])
+                            line_file_id = int(jsp_piece[0].split("#")[1])
+                        else:
+                            input_start_line = int(jsp_piece[0])
+                        if len(jsp_piece) > 1:
+                            repeat_count = int(jsp_piece[1])
+                        output_start_line = int(java_piece[0])
+                        if len(java_piece) > 1:
+                            output_line_increment = int(java_piece[1])
+
+                        if line_file_id is not None:
+                            context = line_file_id
+
+                        if repeat_count is None:
+                            repeat_count = 1
+
+                        if output_line_increment is None:
+                            output_line_increment = 1
+
+                        #print("{}#{},{}:{},{}".format(input_start_line, line_file_id, repeat_count, output_start_line, output_line_increment))
+                        for n in range(0, repeat_count):
+                            offset = 0
+                            for x in range(0, context):
+                                offset += files[x][1]
+
+                            index = input_start_line + n + offset
+                            out = output_start_line + (n*output_line_increment)
+
+                            virt_jsp[index] = (out, virt_jsp[index][1])
+
+            break
+    return virt_jsp
 
 
 def main():
@@ -58,20 +146,14 @@ def main():
 
     # Now have two dicts, one of JSP names and paths and another
     # of smap names and paths
-    # for name in smaps.keys():
-    #    with smaps[name].open() as file:
-    #        for line in file:
-    #            print(line.strip())
-
-    for jsp in jsps.keys():
-        with jsps[jsp].open() as file:
-            print("FILE: "+jsp)
-            for line in file:
-                if ".jsp" in line and "include" in line:
-                    matches = re.search(r"[\'\"](.*.jsp)", line)
-                    if matches:
-                        print(matches.group(1))
-
+    for jsp_name in jsps:
+        print(SEP)
+        print("File: {}".format(jsp_name))
+        print(SEP)
+        virt_jsp = load_jsp(jsp_name, jsps)
+        mapped_virt_jsp = load_smap_jsp(jsp_name, virt_jsp, smaps, jsps)
+        printVirtFile(mapped_virt_jsp)
+        print("\n\n")
 
 
 if __name__ == '__main__':
